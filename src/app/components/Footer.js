3f782c9a148b0d2d05152f4b61212d3bc8e2942c
Felipe Salamanca <juanf.salamancag@konradlorenz.edu.co>
2024-06-26 14:52:26 -0500

    Estilización del componente header

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import '';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
     return(
         <footer>
             <div className='politicas-container'>
                 <ul>
-                    <li><Link to="/">Politicas de tratamientos de datos personales</Link></li>
-                    <li><Link to="/">Politicas de privacidad</Link></li>
-                    <li><Link to="/">Politicas de garantia</Link></li>
-                    <li><Link to="/">Terminos y condiciones</Link></li>
+                    <li><Link href="/">Politicas de tratamientos de datos personales</Link></li>
+                    <li><Link href="/">Politicas de privacidad</Link></li>
+                    <li><Link href="/">Politicas de garantia</Link></li>
+                    <li><Link href="/">Terminos y condiciones</Link></li>
                 </ul>
             </div>
             <div className='copyright-container'>
                 <p>2023 Creado por Sú Instalador</p>
             </div>
             <div className='logo-container'>
-                <image src='' alt='' className=''/>
+                <Image src='' alt='' className=''/>
             </div>
             <div className='Redes Sociales'>
-                <image src='' alt='' className=''/>
-                <image src='' alt='' className=''/>
-                <image src='' alt='' className=''/>
-                <image src='' alt='' className=''/>
+                <Image src='' alt='' className=''/>
+                <Image src='' alt='' className=''/>
+                <Image src='' alt='' className=''/>
+                <Image src='' alt='' className=''/>
             </div>
         </footer>
     )
-}
+};
+
+export default Footer;
--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import "@/app/styles/menu.css"
 const Menu = () => {
   return (
     <header className="header-container">
-      <div className='advertising-container'></div>
+      <div className='advertising-container'>
+        <p>Servicio al cliente - Whatsapp</p>
+      </div>
+      <div className='main-header-container'>
       <div className="logo-container">
-        <img src="src/app/img/LogoSuInstalador.webp" alt="LogoPrincipal" className="logo" />
+        <Image src="/images/LogoSuInstalador.webp" alt="LogoPrincipal" className="logo" width={500} height={300}/>
       </div>
       <div className="search-input-container">
         <input 
@@ -15,21 +19,22 @@ const Menu = () => {
           className="search-input"
         />
         <button className="search-button">
-          <img src="../img/buscar.png" alt="Buscar" className="search-icon" />
+          <Image src="/images/buscar.png" alt="Buscar" className="search-icon" width={500} height={300}/>
         </button>
       </div>
       <nav className="nav-container">
         <ul className="nav-list">
           <li className="nav-item"><Link href="/nosotros">Nosotros</Link></li>
           <li className="nav-item"><Link href="/testimonios">Testimonios</Link></li>
-          <li className="nav-item">
+          <li className="carrito-container">
             <Link href="/carrito">
-              <img src="src/app/img/carro-vacio.png" alt="Carrito" className="cart-icon" />
+              <Image src="/images/carro-vacio.png" alt="Carrito" className="cart-icon" width={42} height={42}/>
               <p className='carrito-number'>0</p>
             </Link>
           </li>
         </ul>
       </nav>
+      </div>
     </header>
   );
 };
